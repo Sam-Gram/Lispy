9d1ea8d8c6f8e0d06f8cdb8d70a7149805276a3f
Sam Graham <samgraham@outlook.com>
2020-05-15 00:08:10 -0600

    portability

--- prompt.c
+++ prompt.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
+#ifdef _WIN32
+static char buffer[2048];
+
+char* readline(char* prompt) {
+  fputs(prompt, stdout);
+  fgets(buffer, 2048, stdin);
+  char* cpy = malloc(strlen(buffer)+1);
+  strcpy(cpy, buffer);
+  cpy[strlen(cpy)-1] = '\0';
+  return cpy
+}
+#else
 #include <editline/readline.h>
 #include <editline/history.h>
-
+#endif
 int main(int argc, char** argv) {
 
   /* Print Version and Exit Information */
@@ -14,7 +28,10 @@ int main(int argc, char** argv) {
 
     /* Output our prompt */
     char* input = readline("lispy> ");
+
+#if ! defined (_WIN32)
     add_history(input);
+#endif
 
     printf("No you're a %s\n", input);
 
