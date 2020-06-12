f0ccc698d7baa2030e5087629777868ec6b2fb4b
Sam Graham <samgraham@outlook.com>
2020-06-11 22:45:52 -0600

    Parser parsing

--- parsing.c
+++ parsing.c
@@ -18,7 +18,23 @@ char* readline(char* prompt) {
 #include <editline/readline.h>
 #include <editline/history.h>
 #endif
+
+
 int main(int argc, char** argv) {
+
+  mpc_parser_t* Number = mpc_new("number");
+  mpc_parser_t* Operator = mpc_new("operator");
+  mpc_parser_t* Expr = mpc_new("expr");
+  mpc_parser_t* Lispy = mpc_new("lispy");
+
+  mpca_lang(MPCA_LANG_DEFAULT,
+    "                                                    \
+      number   : /-?[0-9]+/ ;                            \
+      operator : '+' | '-' | '*' | '/' ;                 \
+      expr     : <number> | '(' <operator> <expr>+ ')' ; \
+      lispy    : /^/ <operator> <expr>+ /$/ ;            \
+    ",
+    Number, Operator, Expr, Lispy);
 
   /* Print Version and Exit Information */
   puts("Lispy Version 0.0.0.0.1");
@@ -29,15 +45,17 @@ int main(int argc, char** argv) {
     /* Output our prompt */
     char* input = readline("lispy> ");
 
-#if ! defined (_WIN32)
-    add_history(input);
-#endif
+    #if ! defined (_WIN32)
+      add_history(input);
+    #endif
 
     printf("No you're a %s\n", input);
 
     // Memory management
     free(input);
   }
+
+  mpc_cleanup(4, Number, Operator, Expr, Lispy);
 
   return 0;
 }
